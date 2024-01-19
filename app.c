5cf9fe715f741fc099605c908dbe2522c3648b8e
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-01-19 18:30:02 -0500

    added a new user in main to demo createUser() function

--- app.c
+++ app.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "users.h"
 
 int main(){
-	printf("Hello\n");
+	char* username = "Andrew";
+	char* password = "stars";
+	User* newUser = createUser(username, password);
+	printf("New use named: %s\n", newUser->username);
+	free(newUser->username);
+	free(newUser->password);
+	free(newUser);
 	return(0);
 }

91048d007e475cc280bfae283415bcae3fc9d5f7
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-01-19 17:10:41 -0500

    wrote createUser() function in users.c and updated Makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
-app: app.c
-	gcc app.c -o app
+all: app
+
+users.o: users.c
+	gcc -c -g users.c
+
+app.o: app.c
+	gcc -c -g app.c
+
+app: app.o users.o
+	gcc -o app app.o users.o
 
 clean:
-	rm app
+	rm -f *.o
+
+clobber: clean
+	rm -f app
---
+++ users.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "users.h"
+
+User* createUser(char* username, char* password) {
+
+	// allocate space for new user
+	User* newUser = (User*) malloc(sizeof(User));
+	if (newUser == NULL) {
+		fprintf(stderr, "Error allocating memory for new user\n");
+	};
+	
+	// allocate space for username and try to copy username into username field
+	newUser->username = (char*) malloc(strlen(username) + 1);
+	if (newUser->username == NULL) {
+		fprintf(stderr, "Error allocating memory for new username\n");
+		free(newUser);
+		return NULL;
+	};
+	strcpy(newUser->username, username);
+
+	// allocate space for password and try to copy password into password field
+	newUser->password = (char*) malloc(strlen(password) + 1);
+	if (newUser->password == NULL) {
+		fprintf(stderr, "Error allocating memory for new password\n");
+		free(newUser);
+		return NULL;
+	};
+	strcpy(newUser->password, password);
+
+	return newUser;
+}
